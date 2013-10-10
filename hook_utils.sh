7cb93e66efa2513a5f5be6d2ad580702037c4262
Emil Maskovsky <emil.maskovsky@xiriar.com>
2013-10-11 00:06:12 +0200

    Move Uncrustify hook to a separate script
    This will allow changes in the code style check script without having
    to update all repositories every time.

---
+++ hook_utils.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+#
+
+# Copyright (c) 2013 Xiriar Software (http://www.xiriar.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##
+# @file
+#
+# Git hook utility functions
+#
+# @author     Emil Maskovsky, emil.maskovsky@xiriar.com
+# @date       2013/11/10
+# @copyright  (c) 2013 Xiriar Software (http://www.xiriar.com/)
+##
+
+
+##
+# Canonicalize by recursively following every symlink in every component of the
+# specified filename.  This should reproduce the results of the GNU version of
+# readlink with the -f option.
+#
+# Reference: http://stackoverflow.com/questions/1055671/how-can-i-get-the-behavior-of-gnus-readlink-f-on-a-mac
+canonicalize_filename () {
+    local target_file=$1
+
+    local physical_directory=""
+    local result=""
+
+    # Need to restore the working directory after work
+    pushd $(pwd) > /dev/null
+
+    cd "$(dirname "$target_file")"
+    target_file=$(basename "$target_file")
+
+    # Iterate down a (possible) chain of symlinks
+    while [ -L "$target_file" ]
+    do
+        target_file=$(readlink "$target_file")
+        cd "$(dirname "$target_file")"
+        target_file=$(basename "$target_file")
+    done
+
+    # Compute the canonicalized name by finding the physical path
+    # for the directory we're in and appending the target file
+    physical_directory=$(pwd -P)
+    result="$physical_directory/$target_file"
+
+    # Restore the working directory
+    popd > /dev/null
+
+    echo "$result"
+}
+
+
+##
+# Check whether the given file name matches any extension in the given list
+test_file_ext() {
+    local filename="$1"
+    local filetypes="$2"
+
+    local filetype
+    for filetype in $filetypes
+    do
+        # Try to remove the filetype pattern from the end of the filename
+        #
+        # If the filename matches the filetype (extension), the extension is
+        # removed (and the processed filename is then different from the
+        # original filename).
+        # This should work in all POSIX compatible shells.
+        [ "$filename" != "${filename%$filetype}" ] && return 0
+    done
+
+    # The filename extension does not match any extension off the list
+    return 1
+}
+
+
+# EOF
---
+++ pre-commit-all.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+#
+
+# Copyright (c) 2013 Xiriar Software (http://www.xiriar.com/)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##
+# @file
+#
+# Git pre-commit hook that runs multiple hooks specified in $HOOKS
+#
+# @author     Emil Maskovsky, emil.maskovsky@xiriar.com
+# @date       2013/11/10
+# @copyright  (c) 2013 Xiriar Software (http://www.xiriar.com/)
+##
+
+
+# Exit and fail on error immediately
+set -e
+
+
+# ============================================================================ |
+# CONFIGURE
+# ============================================================================ |
+
+# Pre-commit hooks to be executed
+#
+# They should be in the same folder as this script.
+# Hooks should return 0 if successful and nonzero to cancel the commit.
+# They are executed in the order in which they are listed.
+#HOOKS="default uncrustify"
+HOOKS="default uncrustify"
+
+
+# ============================================================================ #
+# EXECUTE
+# ============================================================================ #
+
+# Source the utility script
+source "$(dirname "$0")/hook_utils.sh"
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT="$(canonicalize_filename "$0")"
+
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH="$(dirname "$SCRIPT")"
+
+for hook in $HOOKS
+do
+    echo "Running hook: $hook"
+    # run hook if it exists and is executable
+    # if it returns with nonzero exit with 1 and thus abort the commit
+    if [ -x "$SCRIPTPATH/pre-commit-$hook.sh" ]
+    then
+        "$SCRIPTPATH/pre-commit-$hook.sh" || exit 1
+    else
+        echo "Error: file pre-commit-$hook.sh not found."
+        echo "Aborting commit. Make sure the hook is in $SCRIPTPATH and executable."
+        echo "You can disable it by removing it from the list in $SCRIPT."
+        echo "You can skip all pre-commit hooks with --no-verify (not recommended)."
+        exit 1
+    fi
+done
+
+# EOF
