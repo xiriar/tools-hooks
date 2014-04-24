3a528f492f2e4445c90777c3412677fc79a5ac1c
Emil Maskovsky <emil.maskovsky@xiriar.com>
2014-04-24 22:15:11 +0200

    Improve support of directory names with spaces

--- hook_utils.sh
+++ hook_utils.sh
@@ -40,7 +40,7 @@ canonicalize_filename () {
     local result=""
 
     # Need to restore the working directory after work
-    pushd $(pwd) > /dev/null
+    pushd "$(pwd)" > /dev/null
 
     cd "$(dirname "$target_file")"
     target_file=$(basename "$target_file")
